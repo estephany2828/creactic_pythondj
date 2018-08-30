5e77ad0cfeadafc8915133c53063dc1462005ceb
estephany2828 <ejargoty@unicauca.edu.co>
2018-08-30 12:47:27 -0500

    version300820181

--- tienda/settings.py
+++ tienda/settings.py
@@ -81,12 +81,14 @@ WSGI_APPLICATION = 'tienda.wsgi.application'
 
 DATABASES = {
    'default': {
-       'ENGINE': 'django.db.backends.mysql',
-       'NAME': 'tienda',
-       'USER': 'root',
-       'PASSWORD': '',
-       'HOST':'127.0.0.1',
-       'PORT': '3306'
+       #'ENGINE': 'django.db.backends.mysql',
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':'db.sqlite3'
+       #'NAME': 'tienda',
+       #'USER': 'root',
+       #'PASSWORD': '',
+       #'HOST':'127.0.0.1',
+       #'PORT': '3306'
    }
 }
 
